365c11057f5329fd8ed2eb588682740e91b28a41
CristianAva20 <1630396@upv.edu.mx>
2019-01-24 15:33:50 -0600

    First commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,27 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-01-22 15:37:07 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-01-22T15:37:07+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-22T15:37:19+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-22T15:37:27+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-22T15:38:13+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-22T15:38:22+00:00	INFO ::1	joomlafailure	Empty password not allowed.
+2019-01-22T15:38:25+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-22T15:39:46+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-22T15:39:53+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-22T15:40:02+00:00	INFO ::1	joomlafailure	Empty password not allowed.
+2019-01-22T15:41:47+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-22T15:41:57+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-22T15:42:06+00:00	INFO ::1	joomlafailure	Empty password not allowed.
+2019-01-22T15:42:17+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-22T15:44:09+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-23T04:00:56+00:00	INFO ::1	joomlafailure	Empty password not allowed.
+2019-01-23T04:01:05+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-23T04:06:50+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-23T04:07:03+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-23T04:23:50+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-23T20:25:57+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-23T20:29:47+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,17 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-01-22 15:33:23 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-01-22T15:33:23+00:00	INFO ::1	update	COM_JOOMLAUPDATE_UPDATE_LOG_DELETE_FILES
+2019-01-23T04:39:38+00:00	INFO ::1	update	Update started by user Super User (12). Old version is 3.6.2.
+2019-01-23T04:39:38+00:00	INFO ::1	update	Downloading update file from https://downloads.joomla.org/cms/joomla3/3-6-5/Joomla_3.6.5-Stable-Update_Package.zip.
+2019-01-23T04:39:47+00:00	INFO ::1	update	File Joomla_3.6.5-Stable-Update_Package.zip successfully downloaded.
+2019-01-23T04:39:48+00:00	INFO ::1	update	Starting installation of new version.
+2019-01-23T04:42:25+00:00	INFO ::1	update	Finalising installation.
+2019-01-23T04:42:26+00:00	INFO ::1	update	Ran query from file 3.6.3-2016-08-15. Query text: ALTER TABLE `#__newsfeeds` MODIFY `link` VARCHAR(2048) NOT NULL;.
+2019-01-23T04:42:26+00:00	INFO ::1	update	Ran query from file 3.6.3-2016-08-16. Query text: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2019-01-23T04:42:26+00:00	INFO ::1	update	Deleting removed files and folders.
+2019-01-23T04:42:35+00:00	INFO ::1	update	Cleaning up after installation.
+2019-01-23T04:42:35+00:00	INFO ::1	update	Update to version 3.6.5 is complete.
